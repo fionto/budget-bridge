978c78a273174b86f5d760a9a08b16b55573daea
fionto <251855098+fionto@users.noreply.github.com>
2026-07-29 13:23:24 +0200

    testing

--- src/tests.py
+++ src/tests.py
@@ -2,6 +2,8 @@ import models
 
 string_fields = models.Headers.target_names_from_dtype('string')
 original_headers = models.Headers.original_headers()
+rename_map = models.Headers.rename_map()
 
 print(string_fields)
 print(original_headers)
+print(rename_map)
